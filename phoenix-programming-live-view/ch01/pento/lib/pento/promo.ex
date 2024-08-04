c861fe1d097660a2ddcd0186861bcbcb2931592c
James Carr <james.r.carr@gmail.com>
2024-08-04 07:27:47 -0500

    Add a promo code intake form

---
+++ phoenix-programming-live-view/ch01/pento/lib/pento/promo.ex
@@ -0,0 +1,16 @@
+defmodule Pento.Promo do
+  alias Swoosh.Email.Recipient
+  alias Pento.Promo.Recipient
+
+  def change_recipient(%Recipient{} = recipient, attrs \\ %{}) do
+    Recipient.changeset(recipient, attrs)
+  end
+
+  def send_promo(_recipient, _attrs) do
+    # send email to promo recipient
+    #exercise for the reader
+    {:ok, %Recipient{}}
+  end
+
+
+end
---
+++ phoenix-programming-live-view/ch01/pento/lib/pento_web/live/promo_live.ex
@@ -0,0 +1,31 @@
+defmodule PentoWeb.PromoLive do
+  alias Swoosh.Email.Recipient
+  use PentoWeb, :live_view
+  alias Pento.Promo
+  alias Pento.Promo.Recipient
+
+  def mount(_params, _session, socket) do
+    {:ok,
+      socket
+      |> assign_recipient()
+      |> clear_form()}
+  end
+
+  def assign_recipient(socket) do
+    socket
+    |> assign(:recipient, %Recipient{})
+  end
+
+  def clear_form(socket) do
+    form =
+      socket.assigns.recipient
+      |> Promo.change_recipient()
+      |> to_form
+
+    assign(socket, :form, form)
+  end
+
+  def assign_form(socket, changeset) do
+    assign(socket, :form, to_form(changeset))
+  end
+end
