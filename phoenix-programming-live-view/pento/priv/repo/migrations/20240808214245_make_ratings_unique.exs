cd440e8969d0948ac50c6fae6dbfa9b645b13781
James Carr <james.r.carr@gmail.com>
2024-08-08 17:11:51 -0500

    Add migrations for the new ratings and demographics table

---
+++ phoenix-programming-live-view/pento/priv/repo/migrations/20240808214245_make_ratings_unique.exs
@@ -0,0 +1,8 @@
+defmodule Pento.Repo.Migrations.MakeRatingsUnique do
+  use Ecto.Migration
+
+  def change do
+    create unique_index(:ratings, [:user_id, :product_id],
+      name: :index_ratings_on_user_product)
+  end
+end
