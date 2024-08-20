631511aebb5b8dd087763d0d31bcc19ef5859008
James Carr <james.r.carr@gmail.com>
2024-08-19 21:00:17 -0500

    doing this over

--- dynamic_inputs/lib/dynamic_inputs/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule DynamicInputs.Mailer do
-  use Swoosh.Mailer, otp_app: :dynamic_inputs
-end
--- dynamic_inputs/lib/dynamic_inputs/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule DynamicInputs.Repo do
-  use Ecto.Repo,
-    otp_app: :dynamic_inputs,
-    adapter: Ecto.Adapters.Postgres
-end
