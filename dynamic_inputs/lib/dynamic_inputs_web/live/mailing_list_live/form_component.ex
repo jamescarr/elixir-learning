9f8b1756c7bcbfe120ab3140516e35a776681e60
James Carr <james.r.carr@gmail.com>
2024-08-19 21:31:57 -0500

    Make the form more compact

--- dynamic_inputs/lib/dynamic_inputs_web/live/mailing_list_live/form_component.ex
+++ dynamic_inputs/lib/dynamic_inputs_web/live/mailing_list_live/form_component.ex
@@ -6,45 +6,61 @@ defmodule DynamicInputsWeb.MailingListLive.FormComponent do
   @impl true
   def render(assigns) do
     ~H"""
-    <div>
-      <.header>
-        <%= @title %>
-        <:subtitle>Use this form to manage mailing_list records in your database.</:subtitle>
-      </.header>
+      <div class="max-w-2xl mx-auto">
+        <.header>
+          <%= @title %>
+          <:subtitle>Manage mailing list records</:subtitle>
+        </.header>
 
-      <.simple_form
-        for={@form}
-        id="mailing_list-form"
-        phx-target={@myself}
-        phx-change="validate"
-        phx-submit="save"
-      >
-        <.input field={@form[:title]} type="text" label="Title" />
+        <.simple_form
+          for={@form}
+          id="mailing_list-form"
+          phx-target={@myself}
+          phx-change="validate"
+          phx-submit="save"
+          class="space-y-4"
+        >
+          <.input field={@form[:title]} type="text" label="Title" />
 
-        <.inputs_for :let={ef} field={@form[:emails]}>
-          <input type="hidden" name="mailing_list[emails_sort][]" value={ef.index} />
-          <.input type="text" field={ef[:email]} placeholder="email" />
-          <.input type="text" field={ef[:name]} placeholder="name" />
+          <div class="space-y-2">
+            <label class="block text-sm font-medium text-gray-700">Email List</label>
+            <.inputs_for :let={ef} field={@form[:emails]}>
+              <div class="flex items-center space-x-2">
+                <input type="hidden" name="mailing_list[emails_sort][]" value={ef.index} />
+                <div class="flex-grow flex space-x-2">
+                  <.input type="email" field={ef[:email]} placeholder="Email" class="flex-1 py-1 text-sm" />
+                  <.input type="text" field={ef[:name]} placeholder="Name" class="flex-1 py-1 text-sm" />
+                </div>
+                <button
+                  type="button"
+                  name="mailing_list[emails_drop][]"
+                  value={ef.index}
+                  phx-click={JS.dispatch("change")}
+                  class="text-red-600 hover:text-red-800"
+                >
+                  <.icon name="hero-x-mark" class="w-4 h-4" />
+                </button>
+              </div>
+            </.inputs_for>
+          </div>
+
+          <input type="hidden" name="mailing_list[emails_drop][]" />
+
           <button
             type="button"
-            name="mailing_list[emails_drop][]"
-            value={ef.index}
+            name="mailing_list[emails_sort][]"
+            value="new"
             phx-click={JS.dispatch("change")}
+            class="text-sm text-blue-600 hover:text-blue-800"
           >
-            <.icon name="hero-x-mark" class="w-6 h-6 relative top-2" />
+            + Add email
           </button>
-        </.inputs_for>
 
-        <input type="hidden" name="mailing_list[emails_drop][]" />
-
-        <button type="button" name="mailing_list[emails_sort][]" value="new" phx-click={JS.dispatch("change")}>
-          add more
-        </button>
-        <:actions>
-          <.button phx-disable-with="Saving...">Save Mailing list</.button>
-        </:actions>
-      </.simple_form>
-    </div>
+          <:actions>
+            <.button phx-disable-with="Saving..." class="w-full">Save Mailing List</.button>
+          </:actions>
+        </.simple_form>
+      </div>
     """
   end
 
